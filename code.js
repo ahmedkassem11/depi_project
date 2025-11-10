12c595eb3bb186d6ae7a9db4c83476a484d3ee70
ahmedkassem11 <ahmedkassem2911@gmail.com>
2025-11-10 16:54:23 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+// server.js
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200; // OK
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(Server running at http://localhost:${PORT}/);
+});
